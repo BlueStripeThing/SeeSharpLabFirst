5685710f94e3ccc3c8382f91dafcfa212e04ccaf
Vlad <bluestripething@gmail.com>
2021-11-19 16:48:48 +0300

    Добавьте файлы проекта.

---
+++ FirstLab/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FirstLab
+{
+    class Program
+    {
+        
+        public static Dictionary<int, Record> notebook = new Dictionary<int, Record>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует пре-альфа-альфа версия системы хранения данных людей v0.0.0,5\nДля взаимодействия с системой доступны следующие команды");
+            Console.WriteLine("Добавить - создание новой записи\nИзменить - изменение существующей записи\nУдалить - удаление записи\nПросмотр - просмотр информации в записи\nВсе - просмотр всей базы\nПомощь - напоминание о существущих командах\nЗакончить - для завершения работы");
+            Console.WriteLine("Введие команду для начала работы");
+            while (ChooseAction(Console.ReadLine())) { Console.WriteLine("Введите новую команду"); };
+        }
+
+        public static bool ChooseAction(string action)
+        {
+            Program prog = new Program();
+            switch (action.ToLower())
+            {
+                case "добавить":
+                    prog.Add();
+                    break;
+                case "изменить":
+                    Console.WriteLine("Введите Id записи для изменеия");
+                    prog.Change(int.Parse(Console.ReadLine()));
+                    break;
+                case "удалить":
+                    Console.WriteLine("Введите Id записи для удаления");
+                    prog.Delete(int.Parse(Console.ReadLine()));
+                    break;
+                case "просмотр":  
+                    Console.WriteLine("Введите Id записи для просмотра");
+                    prog.Show(int.Parse(Console.ReadLine()));
+                    break;
+                case "все":
+                    prog.ShowAll();
+                    break;
+                case "закончить":
+                    return false;
+                    break;
+                case "помощь":
+                    Console.WriteLine("Добавить - создание новой записи\nИзменить - изменение существующей записи\nУдалить - удаление записи\nПросмотр - просмотр информации в записи\nВсе - просмотр всей базы\nПомощь - напоминание о существущих командах\nЗакончить - для завершения работы");
+                    break;
+            }
+            Console.WriteLine();
+            return true;
+        }
+
+        public void Add()
+        {
+            //Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine()), Console.ReadLine()
+
+            Console.WriteLine("Введите обязательные поля в указанном порядке, каждое с новой строчки - Имя, Фамилия, Номер, Страна");
+            string tempName = Console.ReadLine();
+            string tempSurame = Console.ReadLine();
+            int tempNumb;
+            while (!int.TryParse(Console.ReadLine(), out tempNumb))
+            {
+                Console.WriteLine("Введите корректное число");
+            }
+            string tempCountry = Console.ReadLine();
+            Record record = new Record(tempName, tempSurame, tempNumb, tempCountry);
+            notebook.Add(record.Id, record);
+        }
+        public void Change(int id)
+        {
+            Console.WriteLine("Для редактирования записи, укажите поле для изменения (Имя/фамилия/отчество/телефон/страна/дата рождения/организация/должность/заметки\nДля отмены редактирования введите слово \"Отмена\"");
+            if (notebook.ContainsKey(id))
+            {
+                notebook[id].Redact(Console.ReadLine()); ;
+            }
+            else Console.WriteLine("Элемента с таким Id не существует");
+        }
+        public void Delete(int id)
+        {
+            if (notebook.ContainsKey(id))
+            {
+                notebook.Remove(id);
+                Console.WriteLine("Элемент с id = " +id+ " удалён");
+            }
+            else Console.WriteLine("Элемента с таким Id не существует");
+        }
+        public void Show(int id)
+        {
+            if (notebook.ContainsKey(id))
+            {
+                Console.WriteLine( notebook[id].ShowAllInfo());
+            }
+            else Console.WriteLine("Элемента с таким Id не существует");
+        }
+        public void ShowAll()
+        {
+            Dictionary<int, Record>.Enumerator it = notebook.GetEnumerator();
+            Console.WriteLine("Id  Фамилия  Имя   Номер");
+            while(it.MoveNext())
+            {
+                Console.WriteLine(it.Current.Value.ToString()); 
+                Console.WriteLine("---------------------------------------------");
+            }
+        }
+
+    }
+}
---
+++ FirstLab/Record.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace FirstLab
+{
+    public class Record
+    {
+        public static int counter = 1;
+        public int Id { set; get; }
+
+        private string secondName;
+        public string SecondName { set { secondName = value; } get {return secondName; } }
+
+        private string firstName;
+        public string FirstName{ set { firstName = value; } get { return firstName; } }
+
+        private string lastName;
+        public string LastName{ set { lastName = value; } get { return lastName; } }
+
+        private int number;
+        public int Number  { set { number = value; } get { return number; } }
+
+        private string country;
+        public string Country { set { country = value; } get { return country; } }
+        private DateTime birthday;
+        public DateTime Birthday { set { birthday = value; } get { return birthday; } }
+        private string organisation;
+        public string Organisation { set { organisation = value; } get { return organisation; } }
+        private string job;
+        public string Job { set { job = value; } get { return job; } }
+        private string others;
+        public string Others { set { others = value; } get { return others; } }
+
+        public Record( string firstName, string secondName, int number, string country)
+        {
+            this.Id = counter;
+            counter++;
+            this.SecondName = secondName;
+            this.FirstName = firstName;
+            this.Number = number;
+            this.Country = country;
+            FillAdditional();
+        }
+        public void FillAdditional()
+        {
+            Console.WriteLine("Желаете ли заполнить необязательную информацию? (Да/Нет)");
+            if (Console.ReadLine().ToLower() == "да")
+            {
+                Console.WriteLine("Введите отчество");
+                this.LastName = Console.ReadLine();
+                Console.WriteLine("Введите дату рождения");
+                {
+                    DateTime tempDate;
+                    if (DateTime.TryParse(Console.ReadLine(), out tempDate))
+                        this.Birthday = tempDate;
+                }
+                Console.WriteLine("Введите название организации");
+                this.Organisation = Console.ReadLine();
+                Console.WriteLine("Введите должность");
+                this.Job = Console.ReadLine();
+                Console.WriteLine("Введите заметки");
+                this.Others = Console.ReadLine();
+            }
+        }
+        //~Record()
+        //{
+        //    counter--;
+        //}
+
+        public override string ToString()
+        {
+            return this.Id + " "+ this.SecondName +" "+ this.FirstName +" "+ this.Number;
+        }
+
+        public void Redact(string field)
+        {
+            
+            switch (field.ToLower())
+            {
+                case "имя":
+                    this.FirstName = Console.ReadLine();
+                    break;
+                case "фамилия":
+                    this.SecondName = Console.ReadLine();
+                    break;
+                case "отчество":
+                    this.LastName = Console.ReadLine();
+                    break;
+                case "телефон":
+                    int temp = 0;
+                    if (int.TryParse(Console.ReadLine(), out temp))
+                        this.Number = temp;
+                    else
+                    {
+                        Console.WriteLine("Введите корректный номер"); Redact("телефон");
+                    }
+                    break;
+                case "страна":
+                    this.Country = Console.ReadLine();
+                    break;
+                case "дата рождения":
+                    DateTime tempDate;
+                    if (DateTime.TryParse(Console.ReadLine(), out tempDate))
+                        this.Birthday = tempDate;
+                    else { Console.WriteLine("Введите корректную дату"); Redact("дата рождения"); }
+                    break;
+                case "организация":
+                    this.Organisation = Console.ReadLine();
+                    break;
+                case "должность":
+                    this.Job = Console.ReadLine();
+                    break;
+                case "заметки":
+                    this.Others = Console.ReadLine();
+                    break;
+                case "отмена":
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели несуществующую команду\nВведите иную команду");
+                     Redact(Console.ReadLine());
+                    break;
+            }
+        }
+
+        public string ShowAllInfo()
+        {
+            string s = "Id - ";
+            s += Id + " ";
+            if (this.FirstName != null || this.FirstName != "") { s +="Имя: "; s += this.FirstName; s += " "; }
+            if (this.SecondName != null || this.SecondName != "") { s += "Фамилия: "; s += this.SecondName; s += " "; }
+            if (this.LastName != null || this.LastName != "") { s += "Отчество: "; s += this.LastName; }
+            s += "\n";
+            if (this.Number != 0) { s += "Номер телефона: "; s += this.Number; s += " "; }
+            if (this.Country != null || this.Country != "") { s += "Страна: "; s += this.Country; s += " "; }
+            if (this.Birthday != DateTime.MinValue) { s += "День рождения: "; s += this.Birthday; }
+            s += "\n";
+            if (this.Organisation != null || this.Organisation != "") { s += "Организация: "; s += this.Organisation; s += " "; }
+            if (this.Job != null || this.Job != "") { s += "Должность: "; s += this.Job; s += " "; }
+            if (this.Others != null || this.Others != "") { s += "Заметки: "; s += this.Others; }
+
+            return s;
+        }
+    }
+}
